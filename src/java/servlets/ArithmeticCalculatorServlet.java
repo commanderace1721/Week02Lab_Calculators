72dcdb18392f3947259beaee6ebe3c65e8e2c116
826847 <826847@SAIT224196IT.ACDM.DS.SAIT.CA>
2020-09-22 11:35:36 -0600

    --Second Commit-- Finished adding all the calculators

--- src/java/servlets/ArithmeticCalculatorServlet.java
+++ src/java/servlets/ArithmeticCalculatorServlet.java
@@ -31,6 +31,7 @@ public class ArithmeticCalculatorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        request.setAttribute("message", "---");
         getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request,response);
     }
 
@@ -45,7 +46,58 @@ public class ArithmeticCalculatorServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       
+        String sign = request.getParameter("sign");
+        String first_number = request.getParameter("first_number");
+        String second_number = request.getParameter("second_number");
+        String regex = "[a-zA-Z_]";
+        request.setAttribute("first_number", first_number);
+        request.setAttribute("second_number", second_number);
+        request.setAttribute("sign", sign);
+        if(first_number == null | first_number.equals("") | second_number == null | second_number.equals("") | first_number.matches(regex) | second_number.matches(regex))
+        {
+           request.setAttribute("message", "Invalid");
+              getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request,response);
+        
+           return;
+        }
+        
+        int num1 = Integer.parseInt(first_number);
+        int num2 = Integer.parseInt(second_number);
+        int total;
+        if(sign.equals("%") && num2 == 0)
+        {
+            request.setAttribute("message", "Invalid");
+            getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request,response);
+            return;
+        }
+        if(sign.equals("+"))
+        {
+           total = num1 + num2;
+           request.setAttribute("message", total);
+           getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request,response);
+           return;
+        }
+        if(sign.equals("-"))
+        {
+           total = num1 - num2;
+           request.setAttribute("message", total);
+           getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request,response);
+           return;
+        }
+       if(sign.equals("*"))
+        {
+           total = num1 * num2;
+           request.setAttribute("message", total);
+           getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request,response);
+           return;
+        }
+       if(sign.equals("%"))
+        {
+           total = num1 / num2;
+           request.setAttribute("message", total);
+           getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request,response);
+           return;
+        }
     }
 
 }
